9f76d31f3f2e8cae2b54707b7003a4845b9eefcb
shravanis30 <salunkeshravani561@gmail.com>
2025-06-15 01:28:51 +0530

    Right angled star pattern

--- Patterns/02_RightAngledStarPattern.cpp
+++ Patterns/02_RightAngledStarPattern.cpp
@@ -1,19 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    
-    int n;
+    int n ;
     cout << "Enter the value of n : " << endl;
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << "*" ;
+    for(int i = 1; i <= n ; i ++) {
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
         }
         cout << endl;
     }
 
     return 0;
 }
-
